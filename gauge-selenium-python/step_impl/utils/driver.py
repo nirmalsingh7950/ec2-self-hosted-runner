6c309e543d27100490c070768dc5d8852ba646b4
nirmalsingh7950 <36500490+nirmalsingh7950@users.noreply.github.com>
2020-12-29 12:29:15 +0530

    Update driver.py

--- gauge-selenium-python/step_impl/utils/driver.py
+++ gauge-selenium-python/step_impl/utils/driver.py
@@ -3,8 +3,6 @@ from selenium import webdriver
 
 class Driver(object):
     driver = None
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
     @before_suite
     def __init__():
         Driver.driver = webdriver.Chrome(chrome_options=chrome_options)
