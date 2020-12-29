06b740bc4fa6f50f876e4225d5a2f05e9804e886
nirmalsingh7950 <36500490+nirmalsingh7950@users.noreply.github.com>
2020-12-29 13:18:30 +0530

    Update driver.py

--- gauge-selenium-python/step_impl/utils/driver.py
+++ gauge-selenium-python/step_impl/utils/driver.py
@@ -1,11 +1,16 @@
 from getgauge.python import before_suite, after_suite
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class Driver(object):
     driver = None
+    options = Options()
+    options.headless = True
+    
+    CHROMEDRIVER_PATH='/usr/local/bin/'
     @before_suite
     def __init__():
-        Driver.driver = webdriver.Chrome(chrome_options=chrome_options)
+        Driver.driver = webdriver.Chrome(driver_path=CHROMEDRIVER_PATH, chrome_options=options)
     @after_suite
     def close():
         Driver.driver.close()
