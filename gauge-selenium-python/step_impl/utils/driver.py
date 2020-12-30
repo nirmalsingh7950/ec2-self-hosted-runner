0c5edd1b7c4b2aa28faf38756e517901f544c4b9
nirmalsingh7950 <36500490+nirmalsingh7950@users.noreply.github.com>
2020-12-30 18:11:19 +0530

    Update driver.py

--- gauge-selenium-python/step_impl/utils/driver.py
+++ gauge-selenium-python/step_impl/utils/driver.py
@@ -2,10 +2,20 @@ from getgauge.python import before_suite, after_suite
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 chromeOptions = Options()
-chromeOptions.add_argument("--headless")
-chromeOptions.add_argument('--no-proxy-server')
-#chromeOptions.add_argument("--no-sandbox")
-#chromeOptions.add_argument("--disable-gpu")
+chromeOptions.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) 
+chromeOptions.add_argument("--no-sandbox") 
+chromeOptions.add_argument("--disable-setuid-sandbox") 
+
+chromeOptions.add_argument("--remote-debugging-port=9222")  # this
+
+chromeOptions.add_argument("--disable-dev-shm-using") 
+chromeOptions.add_argument("--disable-extensions") 
+chromeOptions.add_argument("--disable-gpu") 
+chromeOptions.add_argument("start-maximized") 
+chromeOptions.add_argument("disable-infobars") 
+chromeOptions.add_argument("--headless") 
+chromeOptions.add_argument(r"user-data-dir=.\cookies\\" + login) 
+b = webdriver.Chrome(chrome_options=chromeOptions) 
 
 class Driver(object):
     driver = None
