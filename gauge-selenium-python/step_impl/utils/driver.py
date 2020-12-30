e885fef436653a927c1154b9c6b404ae1a1a8dd0
Ubuntu <ubuntu@ip-172-31-24-70.ec2.internal>
2020-12-30 12:12:11 +0000

    changes to driver.py

--- gauge-selenium-python/step_impl/utils/driver.py
+++ gauge-selenium-python/step_impl/utils/driver.py
@@ -1,15 +1,17 @@
 from getgauge.python import before_suite, after_suite
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+chromeOptions = Options()
+chromeOptions.add_argument("--headless")
+chromeOptions.add_argument("--no-sandbox")
+chromeOptions.add_argument("--disable-gpu")
 
 class Driver(object):
     driver = None
-    options = Options()
-    options.headless = True
-    
+  
     @before_suite
-    def __init__():
-        Driver.driver = webdriver.Chrome(chrome_options=options)
+    def init():
+        Driver.driver = webdriver.Chrome(chrome_options=chromeOptions)
     @after_suite
     def close():
         Driver.driver.close()
